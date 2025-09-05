b421b4037b7e5392db801d58875f05b56acf33ba
elprofetrevi <elprofetrevi@gmail.com>
2025-09-05 09:56:32 -0600

    jgkjgkjgkj

--- prueba.py
+++ prueba.py
@@ -4,3 +4,10 @@ print(frase[1:5])
 print(frase[:5])
 print(frase[4:])
 print(frase[::-1])
+
+frase1 = input("Favor de teclear una frase")
+frase2 = frase1.lower().replace(" ","")
+if frase2 == frase2[::-1]:
+    print(frase1 + " es un palindromo")
+else:
+    print(frase1 + " no es un palindromo")
